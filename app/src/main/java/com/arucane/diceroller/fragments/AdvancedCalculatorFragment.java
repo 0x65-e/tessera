f3cd052aab9243c58b956ed456465c515488dddc
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-08-25 21:09:11 -0700

    Copied improved CalculatorFragment parsing code to AdvancedCalculatorFragment

--- app/src/main/java/com/arucane/diceroller/fragments/AdvancedCalculatorFragment.java
+++ app/src/main/java/com/arucane/diceroller/fragments/AdvancedCalculatorFragment.java
@@ -23,6 +23,9 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Calculator that contains calculator-like buttons to create complicated dice codes with modifiers
@@ -30,11 +33,14 @@ import java.util.List;
  */
 public class AdvancedCalculatorFragment extends Fragment {
 
-    List<String> terms = new ArrayList<>();
+    List<DiceGroup> terms;
+    StringBuilder previewText;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.advanced_calculator_fragment, container, false);
+        terms = new ArrayList<>();
+        previewText = new StringBuilder();
 
         // Dice buttons
         final Button d2 = view.findViewById(R.id.advcalc_d2);
@@ -82,114 +88,189 @@ public class AdvancedCalculatorFragment extends Fragment {
         final Button greater_than_equal = view.findViewById(R.id.advcalc_greaterthan_equal);
         final Button less_than_equal = view.findViewById(R.id.advcalc_lessthan_equal);
 
+        calc_roll.setEnabled(false); // No rolling before any buttons are pressed
+        calc_plus.setEnabled(false); // Can't start with a plus either
+
         // TODO: backspace button?
 
-        Button[] simpleButtons = { d2, d3, d4, d6, d8, d10, d12, d20, d100,
-                calc0, calc1, calc2, calc3, calc4, calc5, calc6, calc7, calc8, calc9 };
+        // Have to be atomic so that they can be modified in onClickListeners
+        AtomicReference<ButtonTypes> lastPressed = new AtomicReference<>(ButtonTypes.OPERATION);
+        AtomicBoolean isDN = new AtomicBoolean(false);
+        AtomicInteger modifier = new AtomicInteger(1);
 
-        for (Button b : simpleButtons) {
-            b.setOnClickListener(buttonView -> {
-                // Append the direct text to the last term, to be parsed later
-                appendToLastTerm(((TextView)buttonView).getText());
+        final Button[] simpleDice = { d2, d3, d4, d6, d8, d10, d12, d20, d100 };
+        final int[] simpleDiceValues = { 2, 3, 4, 6, 8, 10, 12, 20, 100 };
+        final Button[] simpleCalc = { calc0, calc1, calc2, calc3, calc4, calc5, calc6, calc7, calc8, calc9 };
+        final Button[] operators = { calc_plus, calc_minus };
+        final int[] operatorValues = { 1, -1 };
+        // Arrays to make enable/disable easier
+        final Button[] dNArray = { dN };
+        final Button[] rollArray = { calc_roll };
+
+        for (int i = 0; i < simpleDice.length; i++) {
+            // effectively final local variables for onClickListener
+            int dieMax = simpleDiceValues[i];
+            simpleDice[i].setOnClickListener(buttonView -> {
+                // Save the dN if we're working on one
+                if (isDN.get()) {
+                    previewText.append("+");
+                    terms.get(terms.size()-1).setMaxVal(modifier.get());
+                    modifier.set(1);
+                    isDN.set(false);
+                }
+                // Add a plus if we have two dice in a row
+                if (lastPressed.get() == ButtonTypes.DIE) {
+                    previewText.append("+");
+                    modifier.set(1);
+                }
+                terms.add(new DiceGroup(modifier.get(), dieMax));
+                lastPressed.set(ButtonTypes.DIE);
+                // enable all buttons
+                enableButtons(simpleDice, dNArray, simpleCalc, operators, rollArray);
                 // Update the preview
+                previewText.append(((TextView)buttonView).getText());
                 TextView preview = view.findViewById(R.id.advcalc_preview);
-                updatePreview(preview);
+                preview.setText(previewText.toString());
             });
         }
 
-        // special version for dN since we don't want to append the underscore
+        for (int i = 0; i < simpleCalc.length; i++) {
+            // effectively final local variables for onClickListener
+            int finalI = i; // only works as a shorthand for the numerical value because buttons are in increasing order!
+            simpleCalc[i].setOnClickListener(buttonView -> {
+                switch (lastPressed.get()) {
+                    case DIE:
+                        previewText.append("+");
+                    case DN: // save max val in modifier until a new group is started explicitly
+                        modifier.set(finalI);
+                        break;
+                    case NUMBER: // working on a multi-digit number of rolls or max dN value
+                        // don't care about race conditions between modifier.get() and modifier.set()
+                        // because two onClickHandlers shouldn't be hit fast enough. If they are, then
+                        // just reset and try again.
+                        int currentMod = modifier.get();
+                        if (currentMod < 0) modifier.set(currentMod * 10 - finalI);
+                        else modifier.set(currentMod * 10 + finalI);
+                        break;
+                    case OPERATION:
+                        modifier.set(modifier.get() * finalI); // multiply to copy sign, either -1 or +1
+                        break;
+                }
+                lastPressed.set(ButtonTypes.NUMBER);
+                // disable nothing, enable everything
+                enableButtons(simpleDice, dNArray, simpleCalc, operators, rollArray);
+                // Update the preview
+                previewText.append(finalI);
+                TextView preview = view.findViewById(R.id.advcalc_preview);
+                preview.setText(previewText.toString());
+            });
+        }
+
         dN.setOnClickListener(buttonView -> {
-            // Append the direct text to the last term, to be parsed later
-            appendToLastTerm("d");
+            // Save the previous dN if we're working on one
+            if (isDN.get()) {
+                previewText.append("+");
+                terms.get(terms.size()-1).setMaxVal(modifier.get());
+                modifier.set(1);
+            }
+            // Add a plus if we have two dice in a row
+            if (lastPressed.get() == ButtonTypes.DIE) {
+                previewText.append("+");
+                modifier.set(1);
+            }
+            terms.add(new DiceGroup(modifier.get(), 0)); // assigning zero temporarily
+            isDN.set(true);
+            lastPressed.set(ButtonTypes.DN);
+            // disable other dice buttons and operators and roll, enable numbers (except zero)
+            enableButtons(simpleCalc);
+            disableButtons(simpleDice, dNArray, operators, rollArray);
+            calc0.setEnabled(false); // No dN starting with zero
             // Update the preview
+            previewText.append("d");
             TextView preview = view.findViewById(R.id.advcalc_preview);
-            updatePreview(preview);
+            preview.setText(previewText.toString());
         });
 
-        Button[] termEnding = { calc_plus, calc_minus };
-
-        for (Button b : termEnding) {
-            b.setOnClickListener(buttonView -> {
-                // Adds a new term starting with the sign
-                terms.add(String.valueOf(((TextView)buttonView).getText()));
+        for (int i = 0; i < operators.length; i++) {
+            int val = operatorValues[i];
+            operators[i].setOnClickListener(buttonView -> {
+                // Save the dN if we're working on one
+                if (isDN.get()) {
+                    terms.get(terms.size()-1).setMaxVal(modifier.get());
+                    isDN.set(false);
+                } else if (lastPressed.get() == ButtonTypes.NUMBER) {
+                    terms.add(new DiceGroup(0, modifier.get())); // special case of a static modifier
+                }
+                // Set positive or negative, depending on the button
+                modifier.set(val);
+                lastPressed.set(ButtonTypes.OPERATION);
+                // disable roll and operators, enable dice buttons and numbers
+                enableButtons(simpleDice, dNArray, simpleCalc);
+                disableButtons(rollArray, operators);
                 // Update the preview
+                previewText.append(((TextView)buttonView).getText());
                 TextView preview = view.findViewById(R.id.advcalc_preview);
-                updatePreview(preview);
+                preview.setText(previewText.toString());
             });
         }
 
         // Clear the list completely (and replace with empty term)
         calc_clear.setOnClickListener(buttonView -> {
             terms.clear();
-            terms.add("");
+            isDN.set(false);
+            lastPressed.set(ButtonTypes.OPERATION); // Should be fine as a default value
+            modifier.set(1);
+            // enable all buttons except roll and plus
+            enableButtons(simpleDice, dNArray, simpleCalc, operators);
+            disableButtons(rollArray);
+            calc_plus.setEnabled(false);
             // Update the preview
+            previewText = new StringBuilder();
             TextView preview = view.findViewById(R.id.advcalc_preview);
-            updatePreview(preview);
+            preview.setText(previewText.toString());
         });
 
         calc_roll.setOnClickListener(buttonView -> {
-            StringBuilder expandedResults = new StringBuilder();
-            int sum = 0;
-            boolean valid = true;
-            // Validate terms
-            for (String term : terms) {
-                // Ignore empty terms
-                if (term.equals(""))
-                    continue;
-
-                if (term.endsWith("d")) {
-                    // problem, we need a die to roll
-                    valid = false;
-                    break;
-                }
-                String[] components = term.split("d");
-                if (components.length > 2) {
-                    // problem, we have more than one die code
-                    valid = false;
-                    break;
-                } else if (components.length == 2) {
-                    // Quick fix for codes with blank number of rolls
-                    if (components[0].equals("") || components[0].equals("-") || components[0].equals("+"))
-                        components[0] += "1";
-
-                    int numRolls = Math.abs(Integer.parseInt(components[0]));
-                    int max = Integer.parseInt(components[1]);
-
-                    // Ignore rolls with zero dice
-                    if (numRolls == 0)
-                        continue;
-
-                    // Roll
-                    DiceGroup group = new DiceGroup(numRolls, max);
-                    List<Integer> results = RollEngine.results(group);
-                    // Account for sign
-                    if (term.startsWith("-")) {
-                        expandedResults.append("-");
-                        sum -= RollEngine.sum(results);
-                    } else if (term.startsWith("+")) {
-                        expandedResults.append("+");
-                        sum += RollEngine.sum(results);
-                    } else {
-                        sum += RollEngine.sum(results);
-                    }
-                    expandedResults.append(results.toString());
+            if (lastPressed.get() == ButtonTypes.NUMBER) {
+                if (isDN.get()) {
+                    // finish the current dN
+                    terms.get(terms.size() - 1).setMaxVal(modifier.get());
                 } else {
-                    // Prevent double signs or signs at the end of terms
-                    if (term.equals("+") || term.equals("-")) {
-                        valid = false;
-                        break;
-                    }
-                    // Must be a single string with no d, therefore a static bonus
-                    sum += Integer.parseInt(term);
-                    expandedResults.append(term);
+                    // finish the current static mod
+                    terms.add(new DiceGroup(0, modifier.get()));
                 }
             }
-
-            if (!valid) {
+            if ((isDN.get() && lastPressed.get() != ButtonTypes.NUMBER) || lastPressed.get() == ButtonTypes.OPERATION) { // Should never occur if buttons are enabled correctly
                 Snackbar.make(view, "Invalid Dice Code", Snackbar.LENGTH_SHORT).show();
             } else {
+                StringBuilder expandedResults = new StringBuilder();
+                int sum = 0;
+
+                // Roll results
+                List<List<Integer>> results = RollEngine.results(terms);
+
+                // Create appropriate result string
+                for (int i = 0; i < results.size(); i++) {
+                    int maxVal = terms.get(i).getMaxVal();
+                    int numRolls = terms.get(i).getNumRolls();
+                    if (numRolls == 0) {
+                        // Static mod
+                        if (expandedResults.length() > 0 && maxVal >= 0) expandedResults.append("+");
+                        expandedResults.append(maxVal);
+                        sum += maxVal;
+                    } else {
+                        if (numRolls < 0) {
+                            expandedResults.append("-");
+                            sum -= RollEngine.sum(results.get(i));
+                        } else {
+                            if (expandedResults.length() > 0) expandedResults.append("+");
+                            sum += RollEngine.sum(results.get(i));
+                        }
+                        expandedResults.append(results.get(i).toString());
+                    }
+                }
+
                 // inflate the layout of the popup window
-                //LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
                 View popupView = inflater.inflate(R.layout.result_popup, null);
 
                 // create the popup window
@@ -215,26 +296,37 @@ public class AdvancedCalculatorFragment extends Fragment {
                     return true;
                 });
             }
+
+            // Remove an extra term from unfinished modifier
+            if (lastPressed.get() == ButtonTypes.NUMBER) {
+                if (isDN.get()) {
+                    // reset the dN. Not necessary, but it may help later for parsing
+                    terms.get(terms.size() - 1).setMaxVal(0);
+                } else {
+                    terms.remove(terms.size() - 1);
+                }
+            }
         });
 
         return view;
     }
 
-    private void appendToLastTerm(CharSequence str) {
-        String term = "";
-        if (terms.size() > 0)
-            term = terms.remove(terms.size() - 1);
-        terms.add(term + str);
+    private static void disableButtons(Button[]... buttons) {
+        for (Button[] barray : buttons) for (Button b : barray) b.setEnabled(false);
     }
 
-    private void updatePreview(TextView preview) {
-        StringBuilder sb = new StringBuilder();
-        for (CharSequence cs : terms)
-            sb.append(cs);
-        preview.setText(sb.toString());
+    private static void enableButtons(Button[]... buttons) {
+        for (Button[] barray : buttons) for (Button b : barray) b.setEnabled(true);
     }
 
     public static Fragment getInstance() {
         return new AdvancedCalculatorFragment();
+    }
+
+    private enum ButtonTypes {
+        NUMBER,
+        DIE,
+        DN,
+        OPERATION
     }
 }
